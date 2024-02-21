7c6a36dfec6fdd56b9ac9f108957917285082e1e
evgenyagantaev <agantaev@mail.ru>
2024-02-21 10:33:19 +0300

    tested with localhost; works well;

---
+++ vps_relay_udp_server.py
@@ -0,0 +1,24 @@
+﻿
+import socket
+
+def relay_messages(server_socket):
+    data, receiver_addr = server_socket.recvfrom(1024)  # Получаем данные и адрес клиента
+    print(f"Received message from {receiver_addr}: {data.decode()}")
+    print("Waiting data from transmitter...")
+
+    while True:
+        data, addr = server_socket.recvfrom(1024)  # Получаем данные и адрес клиента
+        print(f"Received message from {addr}: {data.decode()}")
+        
+        server_socket.sendto(data, receiver_addr)
+        print(f"Relayed message to {receiver_addr}")
+                    
+
+if __name__ == "__main__":
+    # Запрос номера порта у пользователя
+    # port = input("Please enter the port number to bind to: ")
+    # port = int(port)  # Преобразование введенного значения в целое число
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.bind(('', 11373))  
+        print("Relay Server is running. Waiting for test message from receiver...")
+        relay_messages(server_socket)
